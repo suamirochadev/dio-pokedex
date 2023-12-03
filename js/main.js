db987971875fb7d6590052d8b94c3000c27b9c74
Suami Rocha <suamirochati@gmail.com>
2023-12-03 03:16:56 -0300

    pokedex finish ♥

---
+++ js/main.js
@@ -0,0 +1,106 @@
+
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+
+let offset = 0
+
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+
+        ${pokemon.types.map((type) => `<li class="type ${type}"> ${type} </li>`).join('')}
+            </ol >
+    <img src="${pokemon.photo}"
+        alt="${pokemon.name}">
+    </div>
+            </li >
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtml).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNextPage = offset + limit
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+
+
+// ---
+// const pokemonList = document.getElementById('pokemonList');
+// const loadMoreButton = document.getElementById('loadMoreButton');
+// const cardContent = document.querySelector('.card-content');
+
+// const maxRecords = 151;
+// const limit = 10;
+
+// let offset = 0;
+// let displayedPokemons = 0;
+
+// function convertPokemonToHtml(pokemon) {
+//     return `
+//     <li class="pokemon ${pokemon.type}">
+//         <span class="number">#${pokemon.number}</span>
+//         <span class="name">${pokemon.name}</span>
+
+//         <div class="detail">
+//             <ol class="types">
+//                 ${pokemon.types.map((type) => `<li class="type ${type}"> ${type} </li>`).join('')}
+//             </ol>
+//             <img src="${pokemon.photo}" alt="${pokemon.name}">
+//         </div>
+//     </li>
+//     `;
+// }
+
+// function loadPokemonItems(offset, limit) {
+//     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+//         const newHtml = pokemons.map(convertPokemonToHtml).join('');
+//         pokemonList.innerHTML += newHtml;
+//         displayedPokemons += pokemons.length;
+//         updateCardContent();
+//     });
+// }
+
+// function updateCardContent() {
+//     cardContent.innerHTML = `Número de Pokémons Exibidos: ${displayedPokemons}`;
+// }
+
+// loadPokemonItems(offset, limit);
+
+// loadMoreButton.addEventListener('click', () => {
+//     offset += limit;
+//     const qtdRecordsWithNextPage = offset + limit;
+
+//     if (qtdRecordsWithNextPage >= maxRecords) {
+//         const newLimit = maxRecords - offset;
+//         loadPokemonItems(offset, newLimit);
+//         loadMoreButton.parentElement.removeChild(loadMoreButton);
+//     } else {
+//         loadPokemonItems(offset, limit);
+//     }
+// });
